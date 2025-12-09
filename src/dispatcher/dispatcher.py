f9383a0be65727de4b6e923692a61f073f5b5720
johannes <j.kunow@tu-berlin.de>
2025-12-09 21:25:03 +0100

    implement immediate function invocations in dispatcher

--- src/dispatcher/dispatcher.py
+++ src/dispatcher/dispatcher.py
@@ -2,12 +2,13 @@ import json
 import os
 import logging
 from abc import ABC, abstractmethod
-from datetime import datetime, timezone
-from zoneinfo import ZoneInfo
+from datetime import datetime, timezone, timedelta
 from typing import Dict, List, Optional, Any
 from google.cloud import storage
 from google.cloud import tasks_v2
 from google.protobuf import timestamp_pb2
+
+FUNCTION_URL = "https://sample-bucket-writer-eu-north-2-752774698672.europe-north2.run.app"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -40,18 +41,20 @@ class GoogleCloudStorageScheduleLoader(ScheduleLoader):
         blob = bucket.blob("schedule_" + function_name + ".json")
         return json.loads(blob.download_as_string())
 
-def find_best_slot(deadline_str: str, schedule: dict) -> Optional[dict]:
+def get_loader() -> ScheduleLoader:
+    mode = os.environ.get('SCHEDULE_MODE', 'CLOUD')
+
+    if mode == 'CLOUD':
+        bucket_name = os.environ.get('BUCKET_NAME', "faas-scheduling-us-east1")
+        return GoogleCloudStorageScheduleLoader(bucket_name)
+    else:
+        path = os.environ.get('SCHEDULE_FILE_PATH', "./data/sample/execution_schedule.json")
+        return LocalFileScheduleLoader(path)
+
+def find_best_slot(deadline: datetime, schedule: dict) -> Optional[dict]:
     """
     Finds the region/time with the highest priority before the deadline.
     """
-
-    try:
-        # Parse deadline (assuming ISO 8601 format, e.g., "2023-12-31T23:59:59Z")
-        deadline_dt = datetime.fromisoformat(deadline_str).astimezone(timezone.utc)
-    except ValueError:
-        logging.error("Invalid deadline format. Use ISO 8601.")
-        return None
-
     valid_slots = []
 
     for slot in schedule['recommendations']:
@@ -60,7 +63,7 @@ def find_best_slot(deadline_str: str, schedule: dict) -> Optional[dict]:
             now = datetime.now(timezone.utc)
             
             # Constraint: Must be before deadline
-            if slot_time < deadline_dt and slot_time > now:
+            if slot_time < deadline and slot_time > now:
                 valid_slots.append({
                     **slot,
                     'parsed_time': slot_time
@@ -79,18 +82,8 @@ def find_best_slot(deadline_str: str, schedule: dict) -> Optional[dict]:
     del best_slot['parsed_time']
     
     return best_slot
-
-def get_loader() -> ScheduleLoader:
-    mode = os.environ.get('SCHEDULE_MODE', 'CLOUD')
-
-    if mode == 'CLOUD':
-        bucket_name = os.environ.get('BUCKET_NAME', "faas-scheduling-us-east1")
-        return GoogleCloudStorageScheduleLoader(bucket_name)
-    else:
-        path = os.environ.get('SCHEDULE_FILE_PATH', "./data/sample/execution_schedule.json")
-        return LocalFileScheduleLoader(path)
     
-def schedule_delayed_function(function_url: str, target_time: datetime):
+def schedule_function(function_url: str, target_time: datetime):
     client = tasks_v2.CloudTasksClient()
 
     PROJECT_ID = os.environ.get("PROJECT_ID")
@@ -106,16 +99,26 @@ def schedule_delayed_function(function_url: str, target_time: datetime):
         }
     }
 
-    if target_time > datetime.now(timezone.utc):        
-        timestamp = timestamp_pb2.Timestamp()
-        timestamp.FromDatetime(target_time)
+    timestamp = timestamp_pb2.Timestamp()
+    timestamp.FromDatetime(target_time)
         
-        task['schedule_time'] = timestamp
+    task['schedule_time'] = timestamp
 
     response = client.create_task(request={"parent": parent, "task": task})
     
     logging.info(f"Created task {response.name}")
     logging.info(f"Function will execute at {response.schedule_time}")
+
+def find_optimal_region(deadline: datetime, schedule: dict):
+    hour_bucket = deadline.replace(minute=0, second=0, microsecond=0)
+
+    # Search for matching hour in recommendations
+    bucket_str = hour_bucket.strftime("%Y-%m-%d %H:%M:%S")
+    for slot in schedule['recommendations']:
+        if slot["datetime"] == bucket_str:
+            return slot["region"]
+    
+    raise Exception("No recommendation for time: " + str(deadline) + " found")
 
 
 def handler(event):
@@ -131,20 +134,44 @@ def handler(event):
     logging.info(f"Received event: {event}")
 
     fn_name = event.get('function_name')
-    deadline = event.get('deadline')
+    deadline_str = event.get('deadline')
 
-    if not fn_name or not deadline:
+    if not fn_name or not deadline_str:
         return {"statusCode": 400, "body": "Missing 'function_name' or 'deadline'"}
 
     loader = get_loader()
     schedule = loader.load_schedule(fn_name)
 
-    result = find_best_slot(deadline, schedule)
+    deadline_dt: datetime
+
+    try:
+        # Parse deadline (assuming ISO 8601 format, e.g., "2023-12-31T23:59:59Z")
+        deadline_dt = datetime.fromisoformat(deadline_str).replace(tzinfo=timezone.utc)
+    except ValueError:
+        logging.error("Invalid deadline format. Use ISO 8601.")
+        return None
+
+    if deadline_dt < datetime.now(timezone.utc) + timedelta(minutes=5):
+        try:
+            optimal_region = find_optimal_region(deadline_dt, schedule)
+            result = {
+                "datetime": datetime.now(timezone.utc),
+                "region": optimal_region,
+                "priority": 0
+            }
+        except Exception as e:
+            return {
+            "statusCode": 404,
+            "status": "failed",
+            "message": e
+        }
+    else:
+        result = find_best_slot(deadline_dt, schedule)
 
     logging.info("done")
     if result:
         logging.info(f"Dispatching to {result['region']} at {result['datetime']}")
-        schedule_delayed_function("https://sample-bucket-writer-eu-north-2-752774698672.europe-north2.run.app", datetime.fromisoformat(result['datetime']).astimezone(timezone.utc))
+        schedule_function(FUNCTION_URL, result['datetime'])
         return {
             "statusCode": 200,
             "status": "scheduled",
@@ -163,9 +190,9 @@ def handler(event):
 if __name__ == "__main__":
     from dotenv import load_dotenv
     load_dotenv()
-    event = json.loads("""{
+    event = {
         "function_name": "write_to_bucket",
-        "deadline": "2025-12-20T13:56:00"
-    }""")
+        "deadline": "2025-12-09T20:01:00"
+    }
 
     logging.info(handler(event))
