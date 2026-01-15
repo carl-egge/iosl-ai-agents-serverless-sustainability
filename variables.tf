2555ea7e2fcbe9aef0e39cb6c4637259f193adfd
johannes <j.kunow@tu-berlin.de>
2026-01-15 12:30:40 +0100

    wip

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ variable "agent_region" {
   type = string
   default = "us-east1"
 }
+
+variable "gemini_api_key" {
+  type = string
+  sensitive = true
+}
+
+
+variable "electricitymaps_api_key" {
+  type = string
+  sensitive = true
+}
