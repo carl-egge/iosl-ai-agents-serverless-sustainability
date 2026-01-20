cb6c1c758253879252e34d08ba62df271a1a5048
johannes <j.kunow@tu-berlin.de>
2026-01-20 12:32:43 +0100

    add gcs_bucket variable

--- variables.tf
+++ variables.tf
@@ -1,26 +1,30 @@
 variable "project_id" {
-  type = string
+  type    = string
   default = "iosl-faas-scheduling"
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "us-east1"
 }
 
 variable "gemini_api_key" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "electricitymaps_api_key" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "mcp_api_key" {
-  type = string
+  type      = string
   sensitive = true
+}
+
+variable "gcs_bucket" {
+  type = string
 }
 
 variable "schedule_mode" {
